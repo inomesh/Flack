454347bc3e8ff9de0faa2a4de5adcc84e115fc09
inomesh <nomesh.sehgal@gmail.com>
2020-08-09 14:02:30 +0530

    Fixed localStorage object bug
    includes:-  fixed localStorage users object bug of not setting up the file while loading the page. some description for last commit:-  appending messages for a particular user from the localstorage into the body is done,  added functions in socket.js file for storing messages into localStorage,  updated some of the old functions with a better approach,  chat.css undefined property deleted.

--- static/js/localStorage.js
+++ static/js/localStorage.js
@@ -3,94 +3,94 @@
 document.addEventListener('DOMContentLoaded', ()=>{
 
     //setting the sessionStorage for username
-    const dict = { 
-      //first element
-       //user       //channel_name 
-      "Batman": [
-                  { 
-                  "Channel_First": {
-                          "Date_time - 1":"Messages - 1",
-                          "Date_time - 02":"Messages - 02"
+//    const dict = { 
+//       //first element
+//        //user       //channel_name 
+//       "Batman": [
+//                   { 
+//                   "Channel_First": {
+//                           "Date_time - 1":"Messages - 1",
+//                           "Date_time - 02":"Messages - 02"
   
-                  }  
-                  },
+//                   }  
+//                   },
   
-                  { 
-                      "Channel_Second": {
-                              "Date_time - 2":"Messages - 2"
-                      }  
-                  },
+//                   { 
+//                       "Channel_Second": {
+//                               "Date_time - 2":"Messages - 2"
+//                       }  
+//                   },
   
-                  { 
-                      "Channel_third": {
-                              "Date_time - 3":"Messages - 3"
-                      }  
-                  }, 
+//                   { 
+//                       "Channel_third": {
+//                               "Date_time - 3":"Messages - 3"
+//                       }  
+//                   }, 
               
-                  ],
+//                   ],
   
-      //second element in object
-      "Deadpool": [
-                      { 
-                      "Channel_First": {
-                              "Date_time - 1":"Messages - 1"
-                      }  
-                      },
+//       //second element in object
+//       "Deadpool": [
+//                       { 
+//                       "Channel_First": {
+//                               "Date_time - 1":"Messages - 1"
+//                       }  
+//                       },
       
-                      { 
-                          "Channel_Second": {
-                                  "Date_time - 2":"Messages - 2"
-                          }  
-                      },
+//                       { 
+//                           "Channel_Second": {
+//                                   "Date_time - 2":"Messages - 2"
+//                           }  
+//                       },
       
-                      { 
-                          "Channel_third": {
-                                  "Date_time - 3":"Messages - 3"
-                          }  
-                      }, 
+//                       { 
+//                           "Channel_third": {
+//                                   "Date_time - 3":"Messages - 3"
+//                           }  
+//                       }, 
                   
-                      ],
+//                       ],
       
   
-      //third element in object
-      "Wonder_Women": [
-          { 
-          "Channel_First": {
-                  "Date_time - 1":"Messages - 1"
-          }  
-          },
+//       //third element in object
+//       "Wonder_Women": [
+//           { 
+//           "Channel_First": {
+//                   "Date_time - 1":"Messages - 1"
+//           }  
+//           },
   
-          { 
-              "Channel_Second": {
-                      "Date_time - 2":"Messages - 2"
-              }  
-          },
+//           { 
+//               "Channel_Second": {
+//                       "Date_time - 2":"Messages - 2"
+//               }  
+//           },
   
-          { 
-              "Channel_third": {
-                      "Date_time - 3":"Messages - 3"
-              }  
-          }, 
+//           { 
+//               "Channel_third": {
+//                       "Date_time - 3":"Messages - 3"
+//               }  
+//           }, 
       
-          ]
+//           ]
   
   
   
       
-  };
+//   };
     
   // chList = ['Lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipisicing', 'elit', 'Fuga', 'tempora', 'aliquam', 'Molestias', 'suscipit', 'praesentium', 'neque', 'blanditiis', 'expedita', 'ratione', 'quia', 'minima']
   const  chList = ['Lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipisicing', 'elit']
 
   // static variable for all storage
-  // const dict = (localStorage.getItem('users') !== null) ? JSON.parse(localStorage.getItem('users')) : {} ;
+  const dict = (localStorage.getItem('users') !== null) ? JSON.parse(localStorage.getItem('users')) : {} ;
   const channelList = (localStorage.getItem('channelList') != null) ? JSON.parse(localStorage.getItem('channelList')) : chList ;
   // const userList = ((localStorage.length !== 0)) ? JSON.parse(localStorage.getItem('userList')) : [];
 
 
    
     localStorage.setItem('channelList', JSON.stringify(channelList))
-//     localStorage.setItem('users', JSON.stringify(dict))
+    localStorage.setItem('users', JSON.stringify(dict))
 });
 
 
