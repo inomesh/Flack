01b6d714693b2f7e66144ee975a4ace91d4b3973
inomesh <nomesh.sehgal@gmail.com>
2020-08-26 02:35:25 +0530

    Updated Requirements.txt
    by using pip freeeze > requirements.txt command.

--- static/js/localStorage.js
+++ static/js/localStorage.js
@@ -2,86 +2,9 @@
 
 document.addEventListener('DOMContentLoaded', ()=>{
 
-    //setting the sessionStorage for username
-//    const dict = { 
-//       //first element
-//        //user       //channel_name 
-//       "Batman": [
-//                   { 
-//                   "Channel_First": {
-//                           "Date_time - 1":"Messages - 1",
-//                           "Date_time - 02":"Messages - 02"
-  
-//                   }  
-//                   },
-  
-//                   { 
-//                       "Channel_Second": {
-//                               "Date_time - 2":"Messages - 2"
-//                       }  
-//                   },
-  
-//                   { 
-//                       "Channel_third": {
-//                               "Date_time - 3":"Messages - 3"
-//                       }  
-//                   }, 
-              
-//                   ],
-  
-//       //second element in object
-//       "Deadpool": [
-//                       { 
-//                       "Channel_First": {
-//                               "Date_time - 1":"Messages - 1"
-//                       }  
-//                       },
-      
-//                       { 
-//                           "Channel_Second": {
-//                                   "Date_time - 2":"Messages - 2"
-//                           }  
-//                       },
-      
-//                       { 
-//                           "Channel_third": {
-//                                   "Date_time - 3":"Messages - 3"
-//                           }  
-//                       }, 
-                  
-//                       ],
-      
-  
-//       //third element in object
-//       "Wonder_Women": [
-//           { 
-//           "Channel_First": {
-//                   "Date_time - 1":"Messages - 1"
-//           }  
-//           },
-  
-//           { 
-//               "Channel_Second": {
-//                       "Date_time - 2":"Messages - 2"
-//               }  
-//           },
-  
-//           { 
-//               "Channel_third": {
-//                       "Date_time - 3":"Messages - 3"
-//               }  
-//           }, 
-      
-//           ]
-  
-  
-  
-      
-//   };
-    
-  // chList = ['Lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipisicing', 'elit', 'Fuga', 'tempora', 'aliquam', 'Molestias', 'suscipit', 'praesentium', 'neque', 'blanditiis', 'expedita', 'ratione', 'quia', 'minima']
-  const  chList = ['Lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipisicing', 'elit']
 
+  // const  chList = ['Lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipisicing', 'elit']
+  const chList = []
   // static variable for all storage
   const dict = (localStorage.getItem('users') !== null) ? JSON.parse(localStorage.getItem('users')) : {} ;
   const channelList = (localStorage.getItem('channelList') != null) ? JSON.parse(localStorage.getItem('channelList')) : chList ;
